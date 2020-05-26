5ecd397bb444ca7d7b37814a5b27c04b1117e844
Paul <phubbard@pdx.edu>
2020-05-26 15:29:12 -0700

    Working on parsing

--- index.php
+++ index.php
@@ -6,17 +6,21 @@
  <?php 
     include "sample.php";
     $data = new SimpleXMLElement($xmlstr);
-    echo $data->ATTRIBUTES->ATTRIBUTE[0]->ATTRVALUE[0]->COLVALUE;
+    echo $data->ATTRIBUTES['ATTRIBUTE'][1]['ATTR-VALUE'][0]['COL-VALUE'];
+
+    //----------Taken from https://stackify.com/how-to-log-to-console-in-php/
     function console_log($output, $with_script_tags = true) {
-      $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-  ');';
-      if ($with_script_tags) {
-          $js_code = '<script>' . $js_code . '</script>';
-      }
-      echo $js_code;
-  }
+        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
+        if ($with_script_tags) {
+            $js_code = '<script>' . $js_code . '</script>';
+        }
+        echo $js_code;
+    }
+    //---------end taken from https://stackify.com/how-to-log-to-console-in-php/
+
+
     console_log($data);
-    console_log($data->ATTRIBUTES->ATTRIBUTE[0]->ATTRVALUE[0]);
+    console_log($data->ATTRIBUTES->ATTRIBUTE[1]->ATTR[0]);
  ?>
  </body>
 </html>
