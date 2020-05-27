9e8bcb104c13e1aab092eec9a7c6a7dcaef11bf4
phubbard67 <phubbard@pdx.edu>
2020-05-26 17:11:46 -0700

    FIgured out PHP parsing. Now to figure out data relation and add it to the database!

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
  <?php 
     include "sample.php";
     $data = new SimpleXMLElement($xmlstr);
+    //turns the data to json
     $jsonData = json_encode($xmlstr);
-    // echo $data->ATTRIBUTES->ATTRIBUTE[4]["@attributes"];
-    echo $jsonData;
 
     //----------Taken from https://stackify.com/how-to-log-to-console-in-php/
     function console_log($output, $with_script_tags = true) {
@@ -20,10 +19,33 @@
     }
     //---------end taken from https://stackify.com/how-to-log-to-console-in-php/
 
-
+    //------------------------------------------Examples
+    //shows the object
     console_log($data);
+
+    //shows the json output
     console_log($jsonData);
+
+    //shows what a 'name' attribute object contains
     console_log($data->ATTRIBUTES->ATTRIBUTE[4]);
+
+    //gets all the attributes for a particular xml object
+    //example shows how to get the ITEM-TYPE, NAME and DATA-TYPE in a foreach 
+    foreach($data->ATTRIBUTES->ATTRIBUTE[4]->attributes() as $a => $b)
+    {
+        $toPrint = $a . '="'. $b . "\"\n";
+        console_log($toPrint);
+    }
+
+    //access attributes individualy
+    //example gets the ITEM_TYPE for a particular ATTRIBUTE
+    console_log($data->ATTRIBUTES->ATTRIBUTE[4]->attributes()->{'ITEM-TYPE'});
+    
+    //get a particular item from an xml object
+    //this example gets one of the author's names in attribute 4
+    console_log($data->ATTRIBUTES->ATTRIBUTE[4]->{'ATTR-VALUE'}[0]->{'COL-VALUE'});
+    //----------------------------------------End Examples
+
  ?>
  </body>
 </html>
